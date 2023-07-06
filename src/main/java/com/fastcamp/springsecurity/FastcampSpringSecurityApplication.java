964065898339097da29f149037969f7b1f985222
suku0307 <suku0307@gmail.com>
2023-07-06 15:16:19 +0900

    initialize Spring Security Practice Project

---
+++ src/main/java/com/fastcamp/springsecurity/FastcampSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.fastcamp.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastcampSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastcamp/springsecurity/FastcampSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcamp.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
